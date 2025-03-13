54e5c0a8bef63da61ae311204d8a986c2c92765a
hardik singh <realhardik18@gmail.com>
2025-03-13 12:50:47 +0530

    added analytics from vercel

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,12 +19,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">      
-    <Analytics/>
+    <html lang="en">          
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics/>
       </body>
     </html>
   );
